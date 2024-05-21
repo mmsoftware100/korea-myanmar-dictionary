069d59dcb477ef9ba4d99f25fbfed0c8f1a2412a
Aung Ko Man <aungkoman@gmail.com>
2024-05-21 13:02:36 +0630

    dictionary completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,6 +9,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Map<String, dynamic>> dictionary = data;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +20,40 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          TextFormField(),
+          /*
+          TextFormField(
+            onChanged: _onChange,
+          ),
+
+           */
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              // controller: _controller,
+              onChanged: _onChange,
+              decoration: InputDecoration(
+                hintText: 'Search for a word',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+              ),
+            ),
+          ),
+
+          SizedBox(height: 8.0,),
+          if(dictionary.isEmpty) Expanded(child: Center(child: Text("No Result Found"),)),
+          if(dictionary.isNotEmpty) Text("${dictionary.length} results found!", style: TextStyle(color: Colors.grey),),
           Expanded(
               child: ListView.separated(
-                  itemBuilder: (context, index) => _card(data[index]),
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (context, index) => _card(dictionary[index]),
                   separatorBuilder: (index, context) => Divider(),
-                  itemCount: data.length
+                  itemCount: dictionary.length
               )
           )
         ],
@@ -30,12 +61,24 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  void _onChange(String str){
+    // filter and set state data
+    // Find the map containing the specified Myanmar word
+    var result = data.where((item) => item["myn_word"].toString().contains(str) ||  item["kr_word"].toString().contains(str)).toList();
+    setState(() {
+      dictionary = result;
+    });
+  }
+
   Widget _card(Map<String, dynamic> meaning){
-    return Column(
-      children: [
-        Text(meaning['myn_word'] ?? ""),
-        Text(meaning['kr_word'] ?? "")
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Expanded(child: Text(meaning['myn_word'] ?? "")),
+          Expanded(child: Text(meaning['kr_word'] ?? ""))
+        ],
+      ),
     );
   }
 }
